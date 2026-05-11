09d00f53e25d216e77a9df85abadb9173b94e27c
dboutilhe-arch <d.boutilhe@gmail.com>
2026-05-11 20:11:01 +0200

    Create state.js

---
+++ js/state.js
@@ -0,0 +1,11 @@
+// Variables Globales de l'état du bunker
+let peer = null; 
+let players = []; 
+let deck = []; 
+let curG = 0; 
+let curSIdx = -1;
+let votes = { oui: 0, non: 0, total: 0, list: [] };
+let state = { survie: 0, crise: 0, oxy: 3, gameOver: false, suffrage: "Aucun" };
+let isProcessingAction = false;
+let lastSentinelle = null;
+let lastGardien = null;
