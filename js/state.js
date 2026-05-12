d095b6039bfed2585dbec0965613855063908d24
dboutilhe-arch <d.boutilhe@gmail.com>
2026-05-12 10:07:54 +0200

    Update state.js

--- js/state.js
+++ js/state.js
@@ -5,10 +5,14 @@ let deck = [];
 let curG = 0; 
 let curSIdx = -1;
 let votes = { oui: 0, non: 0, total: 0, list: [] };
-let state = { survie: 0, crise: 0, oxy: 3, gameOver: false, suffrage: "Aucun" };
+let state = {  survie: 0, 
+               crise: 0, 
+               oxy: 3,  
+               gameOver: false, 
+               suffrage: "Aucun",
+               lastSentinelle: null,
+               lastGardien: null};
 let isProcessingAction = false;
-let lastSentinelle = null;
-let lastGardien = null;
 let currentPhase = "DÉSIGNATION"; // "DÉSIGNATION", "VOTE", "LÉGISLATION_G", "LÉGISLATION_S"
 let currentProposedS = null;      // Nom de la sentinelle proposée
 let currentLegislativeCards = []; // Cartes envoyées au Gardien ou à la Sentinelle
