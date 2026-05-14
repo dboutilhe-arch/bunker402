aabb9b635569d41e33bcff6a2936574a543a9042
dboutilhe-arch <d.boutilhe@gmail.com>
2026-05-14 15:26:42 +0200

    Update handlers.js

--- js/network/handlers.js
+++ js/network/handlers.js
@@ -76,7 +76,6 @@ function handleJoin(conn, data) {
 }
 
 function handleSentinelle(data) {
-    resetVoteColors();
     Logger.add(`CONSEIL : Le Gardien ${data.gardienName} a désigné ${data.sentinelleName}.`);
     const gTags = document.querySelectorAll(`[id="tag-${players[state.curG].name.toLowerCase()}"]`);
     gTags.forEach(tag => {
