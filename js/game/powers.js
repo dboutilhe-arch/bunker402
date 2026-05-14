ad4379d813a01f172744f0fc2a867bb1b4c7c6ce
dboutilhe-arch <d.boutilhe@gmail.com>
2026-05-14 10:54:20 +0200

    Update powers.js

--- js/game/powers.js
+++ js/game/powers.js
@@ -1,1 +1,37 @@
+import { state, players } from '../core/state.js';
+import { POWER_MAP } from '../core/constants.js';
+import { Logger } from '../ui/logger.js';
 
+export function checkCasePower(caseNumber) {
+    const n = players.length;
+    const config = POWER_MAP[n] || POWER_MAP['default'];
+    const power = config[caseNumber];
+
+    if (!power) return;
+
+    currentPowerActive = true;
+    const gardien = players.find(p => p.name === state.lastGardien);
+    addLog(`SYSTÈME : Case de Crise ${caseNumber} atteinte. Activation du protocole : ${power}.`);
+
+    switch (power) {
+        case 'TEST':
+            // On force l'ouverture du sélecteur chez le Gardien
+            gardien.conn.send({ 
+                type: 'FORCE_POWER_SELECT', 
+                action: 'REQUEST_BLOOD_TEST', 
+                title: 'ANALYSE BIOLOGIQUE (DÉCRET)' 
+            });
+            break;
+        case 'EXEC':
+            // À implémenter : openTargetSelector('REQUEST_EXECUTION', 'PROTOCOLE D\'ÉLIMINATION')
+            break;
+        case 'CENSURE':
+            // On force l'ouverture du sélecteur chez le Gardien
+            gardien.conn.send({ 
+                    type: 'FORCE_POWER_SELECT', 
+                    action: 'REQUEST_CENSURE', 
+                    title: 'PROTOCOLE DE CENSURE' 
+                });
+            break;
+    }
+}
