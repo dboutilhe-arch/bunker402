7da9ce962f2829ad0cb7987ee1bb30081830637b
dboutilhe-arch <d.boutilhe@gmail.com>
2026-05-14 10:39:22 +0200

    Update logger.js

--- js/ui/logger.js
+++ js/ui/logger.js
@@ -1,18 +1,1 @@
-// logger.js
-// Gestion du log console
 
-export const Logger = {
-    el: document.getElementById('log'),
-
-    add(message, type = "SYSTEM") {
-        const time = new Date().toLocaleTimeString('fr-FR', { hour: '2-digit', minute: '2-digit', second: '2-digit' });
-        const color = type === "ALERTE" ? "#e74c3c" : "#2ecc71";
-        
-        this.el.innerHTML += `<div>[${time}] <span style="color:${color}">> ${message}</span></div>`;
-        this.el.scrollTop = this.el.scrollHeight;
-    },
-
-    clear() {
-        this.el.innerHTML = "<div>[SYSTÈME] : Redémarrage effectué.</div>";
-    }
-};
