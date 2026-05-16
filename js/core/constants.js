fafe4179a8357c035e97b876cb79037c38f8b8ed
dboutilhe-arch <d.boutilhe@gmail.com>
2026-05-16 12:47:57 +0200

    Update POWER_MAP default case values

--- js/core/constants.js
+++ js/core/constants.js
@@ -24,7 +24,7 @@ export const ROLE_COMPOSITIONS = {
 // Répartition des pouvoirs de case en fonction du nombre de joueur
 export const POWER_MAP = {
     // TEST
-    default: { 1: 'null', 2: 'null', 3: 'null', 4: 'null', 5: 'null' }
+    default: { 1: null, 2: 'CENSURE', 3: 'TEST', 4: 'EXEC', 5: 'EXEC' }
     //default: { 1: 'CENSURE', 2: 'CENSURE', 3: 'CENSURE', 4: 'CENSURE', 5: 'CENSURE' }
 
     // 5 joueurs
