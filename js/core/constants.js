2f3f3ea292ba1240f9c1fc4880ce22009a2cca85
dboutilhe-arch <d.boutilhe@gmail.com>
2026-05-17 23:12:39 +0200

    Update POWER_MAP and JOBS_LIST constants

--- js/core/constants.js
+++ js/core/constants.js
@@ -24,24 +24,24 @@ export const ROLE_COMPOSITIONS = {
 // Répartition des pouvoirs de case en fonction du nombre de joueur
 export const POWER_MAP = {
     // TEST
-    //default: { 1: null, 2: 'CENSURE', 3: 'TEST', 4: 'EXEC', 5: 'EXEC' }
+    default: { 1: null, 2: null, 3: null, 4: null, 5: null }
     //default: { 1: 'CENSURE', 2: 'CENSURE', 3: 'CENSURE', 4: 'CENSURE', 5: 'CENSURE' }
 
     // 5 joueurs
-    5:  { 3: null, 4: 'TEST', 5: 'EXEC' },
+    //5:  { 3: null, 4: 'TEST', 5: 'EXEC' },
     // 6 à 7 joueurs
-    6:  { 3: 'TEST', 4: 'TEST', 5: 'EXEC' },
-    7:  { 3: 'TEST', 4: 'TEST', 5: 'EXEC' },
+    //6:  { 3: 'TEST', 4: 'TEST', 5: 'EXEC' },
+    //7:  { 3: 'TEST', 4: 'TEST', 5: 'EXEC' },
     // 8 à 10 joueurs
-    8:  { 3: 'TEST', 4: 'EXEC', 5: 'EXEC' },
-    9:  { 3: 'TEST', 4: 'EXEC', 5: 'EXEC' },
-    10: { 3: 'TEST', 4: 'EXEC', 5: 'EXEC' },
+    //8:  { 3: 'TEST', 4: 'EXEC', 5: 'EXEC' },
+    //9:  { 3: 'TEST', 4: 'EXEC', 5: 'EXEC' },
+    //10: { 3: 'TEST', 4: 'EXEC', 5: 'EXEC' },
     // 11 joueurs et plus
-    default: { 2: 'CENSURE', 3: 'TEST', 4: 'EXEC', 5: 'EXEC' }
+    //default: { 2: 'CENSURE', 3: 'TEST', 4: 'EXEC', 5: 'EXEC' }
 };
 
 //export const JOBS_LIST = ['Shérif', 'Docteur', 'Technicien', 'Journaliste', 'Militaire', 'Psychologue', 'Contrebandier', 'Fossoyeur', 'Éclaireur', 'Vigile', 'Scientifique', 'Ingénieur', 'Pilote'];
-export const JOBS_LIST = ['Militaire', 'Docteur', 'Intendant']
+export const JOBS_LIST = ['Militaire', 'Docteur', 'Intendant', 'Shérif', 'Fossoyeur']
 export const DEFAULT_JOB = "Sans emploi";
 
 /**
