f9360439280485fbb64aff34992d79f3bfb07888
dboutilhe-arch <d.boutilhe@gmail.com>
2026-05-17 11:46:34 +0200

    Comment out specific entries in INITIAL_DECK_LIST
    Commented out the censure and loi_493 entries in the INITIAL_DECK_LIST.

--- js/core/constants.js
+++ js/core/constants.js
@@ -89,7 +89,9 @@ export const INITIAL_DECK_LIST = [
     // 10 Bleues
     'test_sanguin', 'test_sanguin', 'reorganisation', 'commission', 'purge', 'presse', 'rebellion', 'transparence', 'contre_pouvoir', 'fuite_air_s',
     // 15 Rouges
-    'censure', 'censure', 'censure', 'loi_493', 'loi_493', 'reelection', 'sabotage', 'coup_etat', 'court_circuit', 'licenciement', 'silence', 'prophete', 'talion', 'fuite_air_c', 'secret_etat',
+    //'censure', 'censure', 'censure', 'loi_493', 'loi_493', 'reelection', 'sabotage', 'coup_etat', 'court_circuit', 'licenciement', 'silence', 'prophete', 'talion', 'fuite_air_c', 'secret_etat',
+    //TEST
+    'sabotage', 'sabotage', 'sabotage', 'sabotage', 'sabotage', 'sabotage', 'sabotage', 'sabotage', 'sabotage', 'sabotage', 'sabotage', 'sabotage', 'sabotage', 'sabotage', 'sabotage',
     // 4 Grises
     'conseil_restreint', 'greve_zele', 'appel_nominal', 'piston'
 ];
