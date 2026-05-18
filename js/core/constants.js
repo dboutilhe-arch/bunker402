12350e2c17996155d507927484eff8dc36dbf8eb
dboutilhe-arch <d.boutilhe@gmail.com>
2026-05-18 21:52:15 +0200

    Add support for 6 players in POWER_MAP

--- js/core/constants.js
+++ js/core/constants.js
@@ -24,7 +24,7 @@ export const ROLE_COMPOSITIONS = {
 // Répartition des pouvoirs de case en fonction du nombre de joueur
 export const POWER_MAP = {
     // TEST
-    default: { 1: null, 2: null, 3: null, 4: null, 5: null }
+    default: { 1: null, 2: null, 3: null, 4: null, 5: null, 6: null }
     //default: { 1: 'TEST', 2: 'CENSURE', 3: 'CENSURE', 4: 'CENSURE', 5: 'CENSURE' }
 
     // 5 joueurs
